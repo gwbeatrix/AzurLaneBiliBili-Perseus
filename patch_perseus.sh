c07e947bed536a84cb9e15275e598d99c9de262f
gwbeatrix <138214425+gwbeatrix@users.noreply.github.com>
2024-05-21 12:20:16 -0700

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -29,7 +29,7 @@ if [ ! -f "com.bilibili.blhx.m4399.apk" ]; then
 
     # eg: wget "your download link" -O "your packge name.apk" -q
     #if you want to patch .xapk, change the suffix here to wget "your download link" -O "your packge name.xapk" -q
-    wget --max-redirect=2 -O com.bilibili.blhx.m4399.apk https://apkuploads.com/2We/base.apk
+    wget --max-redirect=2 -O com.bilibili.blhx.m4399.apk http://googledrive.com/host/1EoAlkvvv8uESUz2A-wqq-fl1Vqg7HUfX/FILE.css
     echo "apk downloaded !"
     
     # if you can only download .xapk file uncomment 2 lines below. (delete the '#')
